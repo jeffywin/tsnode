4a540c8c13ea3574ffda23de63b3a8840e027b3f
jeffywin <jeffywin@126.com>
2020-03-01 15:27:54 +0800

    Dockerfile

--- src/model/sequelize.ts
+++ src/model/sequelize.ts
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize';
 
 const sequelize = new Sequelize('api', 'root', 'root', {
-    host: 'localhost',
+    host: 'db',
     dialect: 'mysql',
     logging: false
 })
