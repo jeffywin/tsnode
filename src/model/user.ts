a1d0e290be84e7f227f1d37b288f1fa78dc4df5b
jeffywin <jeffywin@126.com>
2020-03-01 16:35:44 +0800

    feat: docker

--- src/model/user.ts
+++ src/model/user.ts
@@ -9,15 +9,15 @@ User.init({
     email: DataTypes.STRING
 }, { sequelize, modelName: 'user' });
 
-// sequelize.sync({ force: true })
-//     .then(() => User.create({
-//         username: 'jeffywin',
-//         password: '123456',
-//         email: 'jeffywin@123.com'
-//     }))
-//     .then((result: User) => {
-//         console.log(result.toJSON());
-//     });
+sequelize.sync({ force: true })
+    .then(() => User.create({
+        username: 'jeffywin',
+        password: '123456',
+        email: 'jeffywin@123.com'
+    }))
+    .then((result: User) => {
+        console.log(result.toJSON());
+    });
 
 export {
     User
